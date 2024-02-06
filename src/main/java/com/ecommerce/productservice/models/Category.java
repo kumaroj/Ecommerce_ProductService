7dd61ff8951a397ebe8747e7395f8f46dd64a9f7
kumaroj <manishkr2207@gmail.com>
2024-02-07 01:01:50 +0530

    Added category controller class

--- src/main/java/com/ecommerce/productservice/models/Category.java
+++ src/main/java/com/ecommerce/productservice/models/Category.java
@@ -2,8 +2,11 @@ package com.ecommerce.productservice.models;
 
 import jakarta.persistence.Entity;
 
+import java.util.List;
+
 @Entity
 public class Category extends  BaseModel{
 
     String name;
+    List<Product> productList;
 }
